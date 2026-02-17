5792cc6f184e5b3926311cdde8e878af3d83ad7b
josephfoster05 <josephfoster2005@gmail.com>
2026-02-17 15:39:06 +0000

    added

--- app/pages/admin/admin-pages.vue
+++ app/pages/admin/admin-pages.vue
@@ -1,5 +1,36 @@
+<script>
+import { user } from "~~/server/api/use-auth";
+import Header from "../../components/header.vue";
+
+export default {
+  components: {
+    Header,
+  },
+};
+
+</script>
+
 <template>
-    <div>
-        <h1>Welcome to NHS Wayfinder</h1>
-    </div>
+  <Header/>
+  <div>
+    <h1>Welcome to NHS Wayfinder, {{ user?.username || 'Guest' }}</h1>
+    <p>
+      <NuxtLink to="/show-users">View Users</NuxtLink>
+      <NuxtLink to="/list-routes">View Routes</NuxtLink>
+      <NuxtLink to="/add-user">Add User</NuxtLink>
+    </p>
+  </div>
+
+  <h2>ADMIN PANEL</h2>
+  <div>
+    <NuxtLink to="/admin/create-route">Create Route</NuxtLink>
+    <NuxtLink to="/admin/create-routemap">Create Route Map</NuxtLink>
+    <NuxtLink to="/admin/edit-routemap">Edit Route Map</NuxtLink>
+    <NuxtLink to="/admin/list-locations">List Locations</NuxtLink>
+    <NuxtLink to="/admin/list-media">List Media</NuxtLink>
+    <NuxtLink to="/admin/list-routesadmin">List routesadmin</NuxtLink>
+    <NuxtLink to="/admin/qr-code">QR code</NuxtLink>
+    <NuxtLink to="/admin/review-media">Review Media</NuxtLink>
+    <NuxtLink to="/admin/bfs-debug">Bfs Debug</NuxtLink>
+  </div>
 </template>
