5bb874c763d0d26af4f9bc63ea11a0f452432861
ItsJustJoshua <darkfoxjnr123@gmail.com>
2026-02-18 09:48:06 +0000

    added links to dashboard

--- app/pages/admin/dashboard.vue
+++ app/pages/admin/dashboard.vue
@@ -1,5 +1,24 @@
 <template>
     <div>
         <h1>dashboard</h1>
+
+
+        <div>
+            <div class="admin-nav">
+                <ul>
+                    <li><NuxtLink to="/admin/create-route">Create Route</NuxtLink></li>
+                    <li><NuxtLink to="/admin/create-routemap">Create Route Map</NuxtLink></li>
+                    <li><NuxtLink to="/admin/edit-routemap">Edit Route Map</NuxtLink></li>
+                    <li><NuxtLink to="/admin/list-locations">List Locations</NuxtLink></li>
+                    <li><NuxtLink to="/admin/list-media">List Media</NuxtLink></li>
+                    <li><NuxtLink to="/admin/list-routesadmin">List Routes</NuxtLink></li>
+                    <li><NuxtLink to="/admin/qr-code">QR Code</NuxtLink></li>
+                    <li><NuxtLink to="/admin/review-media">Review Media</NuxtLink></li>
+                    <li><NuxtLink to="/admin/bfs-debug">BFS Debug</NuxtLink></li>
+                </ul>
+            </div>
+        </div>
+
+        
     </div>
 </template>
