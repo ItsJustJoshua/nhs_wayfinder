5ba16ee970fa2d0876dfc2e7c7390cd4137e248c
Connor4020 <connor.barton763@gmail.com>
2026-03-24 11:57:07 +0000

    Built the about and contact page.

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,1 +1,72 @@
-<template><main></main></template>
+<template>
+  <main>
+    <div>
+      <h1>About</h1>
+      <p></p>
+    </div>
+
+    <article>
+      <ul>
+        <li>
+          <div>
+            <div>
+              <h2>
+                <p>What does the Wayfinder do?</p>
+              </h2>
+              <div>
+                <p>
+                  This website was designed to help visitors of the Northern
+                  General Hospital get from one location to another using a
+                  pathfinding algorithm in order to reduce reliability on staff
+                  assistance.
+                </p>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+          <div>
+            <div>
+              <h2>
+                <p>How to use the website?</p>
+              </h2>
+              <div>
+                <p>
+                  Simply navigate to the home page, input your current location
+                  and your target destination and select whether you want an
+                  accessible path. Follow the instructions provided on screen to
+                  get to your target location. Ask a member staff for further
+                  assistance.
+                </p>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+          <div>
+            <div>
+              <h2>
+                <p>Data use</p>
+              </h2>
+              <div>
+                <p>
+                  Some personal data like your email adress and password will be
+                  stored safely server side for correct website functionality.
+                  You may request your data to be deleted at any time.
+                </p>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+
+      <div></div>
+    </article>
+  </main>
+</template>
--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -1,1 +1,46 @@
-<template><main></main></template>
+<template>
+  <main>
+    <div>
+      <h1>Contact</h1>
+      <p></p>
+    </div>
+
+    <article>
+      <ul>
+        <li>
+          <div>
+            <div>
+              <h2>
+                <p>Northern General Hospital Phone Number</p>
+              </h2>
+              <div>
+                <p>0114 243 4343</p>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+          <div>
+            <div>
+              <h2>
+                <p></p>
+                Northern General Hospital Website
+              </h2>
+              <div>
+                <a
+                  href="https://www.sth.nhs.uk/find-hospitals-and-services/northern-general-hospital/"
+                  >https://www.sth.nhs.uk/find-hospitals-and-services/northern-general-hospital/</a
+                >
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+
+      <div></div>
+    </article>
+  </main>
+</template>
